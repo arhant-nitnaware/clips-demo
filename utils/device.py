80b8ddc916d2764b836fc3644b9e4364be2c7e0c
arhant nitnaware <arhant00@gmail.com>
2026-06-12 10:32:08 +0530

    cuda prints in device.py'

--- utils/device.py
+++ utils/device.py
@@ -5,6 +5,10 @@ import torch
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 print(f"Using device: {DEVICE}")
+print(torch.cuda.is_available())
+print(torch.cuda.device_count())
+print(torch.version.cuda)
+print(torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No GPU")
 
 torch.set_num_threads(4)
 
